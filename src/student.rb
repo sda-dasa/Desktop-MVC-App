7680a43fec7a307051cb5c6740c78d0c24057cca
Darya Saltykova <saltykovadasa15@gmail.com>
2025-09-20 09:40:18 +0000

    L1 T4.1

---
+++ src/student.rb
@@ -0,0 +1,12 @@
+class Student
+	def initialize (first_name, last_name, surname = nil, phone= nil, tg = nil, mail = nil, git = nil, id = nil) 
+		@first_name = first_name
+		@last_name = last_name
+		@surname = surname
+		@phone = phone
+		@tg = tg
+		@mail = mail
+		@git = git
+		@id = id
+	end
+end
