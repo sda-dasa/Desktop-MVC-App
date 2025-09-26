6fddc521b4ceb71f80bea6fffc350a5b98f79744
Darya Saltykova <saltykovadasa15@gmail.com>
2025-09-26 13:59:23 +0000

    L1 T2.3 V11

--- src/numeric_algorithms.rb
+++ src/numeric_algorithms.rb
@@ -31,25 +31,23 @@ return min_numb
 end
 
 
-
 def have_common_divisors? (numb_1, numb_2)
 comon_divisor = 1
 max = if numb_1 > numb_2 then numb_1 else numb_2 end
-k = 1
+k = 2
 while k <= max and comon_divisor == 1
 	if numb_1 % k == 0 and numb_2 % k == 0
-		then comon_divisor = k
+		comon_divisor = k	
 	end
 k = k + 1
 end
 
-if comon_divisor != 1 and numb_1 != 0 or numb_2 != 0
+if comon_divisor != 1 and numb_1 != 0 and numb_2 != 0
 	then return true
 else return false
 end
 
 end
-
 
 def special_divisors_sum (a)
 sum_of_digits = 0
@@ -62,13 +60,12 @@ while i !=0
 	
 	i = (i - curr_numb) / 10
 end
-i = 1
+i = 2
 sum = 0
-while i <= a
-if a % i == 0
-	then	
+while i < a - 1
+if a % i == 0	
 	if have_common_divisors? product_of_digits, i and not have_common_divisors? sum_of_digits, i
-		then sum = sum + i
+		 sum = sum + i
 	end
 end
 i = i + 1
