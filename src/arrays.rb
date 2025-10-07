2daa1767af4f0c0d3a50467a25af432ba58017db
Darya Saltykova <saltykovadasa15@gmail.com>
2025-10-07 06:52:31 +0000

    L2 T1 V11 the find_unique_element, find_closest_element, squared_frequent_non_negative were updated

--- src/arrays.rb
+++ src/arrays.rb
@@ -1,5 +1,5 @@
 def find_unique_element(array)
-  array.min_by{|el| array.count(el)}
+  result = array.min_by{|el| array.count(el)} != array.max_by{|el| array.count(el)} ? array.min_by{|el| array.count(el)} : nil
 end
 
 def two_min_elements (array)
@@ -8,22 +8,23 @@ end
 
 
 def find_closest_element (array, r)
-  if array.empty? then return 0 end
   array_of_deviation = array.map {|el| (el - r).abs}
-  array[array_of_deviation.find_index(array_of_deviation.min)]
+  index_ = array_of_deviation.find_index(array_of_deviation.min)
+  result = if index_.nil? then nil else  array[index_] end
 end
+
 
 
 def find_divisors(el)
   (1..el).select{|i| el % i == 0}
 end
 
+
 def unique_positive_divisors (array)
-  array.flat_map {|el| find_divisors(el)}.to_set.to_a
+  array.flat_map {|el| find_divisors(el)}.to_set.to_a.sort
 end
 
 
 def squared_frequent_non_negative (array)
-  array.select{|el| el >= 0 && el < 100 && array.count(el) > 2}.to_set.map{|el| el ** 2}
+  array.select{|el| (el.class == Integer || el.class == Float) && el >= 0 && el < 100 && array.count(el) > 2}.to_set.map{|el| el ** 2}
 end
-
