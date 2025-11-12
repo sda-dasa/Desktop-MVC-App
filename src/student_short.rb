4f47ea3996ed373ef02f773498dcd3f4367222d9
sda-dasa <saltykovadasa15@gmail.com>
2025-11-12 17:26:19 +0300

    making StudentShort a child class for StudentBase

--- src/student_short.rb
+++ src/student_short.rb
@@ -1,46 +1,28 @@
-class StudentShort
-	def initialize (id: , last_name_initials: , contact: nil, git: nil)
-		@id = id
-		@last_name_initials  = last_name_initials
-		@contact = contact
-		@git = git
-	end
+require_relative 'student.rb'
+require_relative 'student_base.rb'
 
-	attr_reader :id, :last_name_initials, :contact, :git
-
-	def self.from_student (student)	
-    	raise ArgumentError, "Expected Student, given #{student.class}" unless student.is_a?(Student)
-		new(id: student.id, last_name_initials: student.last_name_initials, contact: student.contact, git: student.git)
-	end
-
-	def short_info
-		student_info(show_all: false)
-	end
-
-	def to_s
-		student_info(show_all: true)
-	end
-
-
-  def student_info(show_all:)
-    result = []
-    result << "id - #{id}" if show_all || id
-    result << "last_name_initials - #{last_name_initials}"
-
-    result << "contact - #{contact}" if show_all || has_contact?
-
-    result << "git - #{git}" if show_all || has_git?
-    result.join("\n")
-
+class StudentShort < StudentBase
+  attr_reader :last_name_initials, :contact
+  
+  def initialize(id:, last_name_initials:, contact: nil, git: nil)
+    super(id: id, git: git)
+    @last_name_initials = last_name_initials
+    @contact = contact
   end
 
-	def has_git?
-		not (git.nil? or git.empty?)
-	end
+  def self.from_student(student)
+    raise ArgumentError, "Expected Student, given #{student.class}" unless student.is_a?(Student)
 
-	def has_contact?
-		not (contact.nil? or contact.empty?)
-	end
-
+    new( id: student.id, last_name_initials: student.last_name_initials, contact: student.contact, git: student.git)
+  end
+  
+  def to_s
+    "
+      id - #{id}
+      fio - #{last_name_initials} 
+      contact - #{contact}
+      git - #{git} 
+    "
+  end
 
 end
