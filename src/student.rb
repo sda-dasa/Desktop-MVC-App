5ce771c29f5d4d4d9c63ce031c8eb90473c85abb
sda-dasa <saltykovadasa15@gmail.com>
2025-11-29 13:53:09 +0300

    L3 T1

--- src/student.rb
+++ src/student.rb
@@ -16,17 +16,7 @@ class Student < StudentBase
   attr_validate_writer :first_name, field_name: "first_name", required: true, with: :valid_name?
   attr_validate_writer :patronymic, field_name: "patronymic", required: false, with: :valid_name?
   attr_validate_writer :git, field_name: "git", required: false, with: :valid_git?
-    
-  # public 
-  # {
-  #   name: NAME_REGEX,
-  #   phone: PHONE_REGEX,
-  #   telegram: TELEGRAM_REGEX,
-  #   email: EMAIL_REGEX,
-  #   git: GIT_REGEX
-  # }.each do |field_name, regex| 
-  #     attr_validate field_name, regex: regex
-  # end
+
   attr_validate :name, regex: NAME_REGEX
   attr_validate :phone, regex: PHONE_REGEX
   attr_validate :telegram, regex: TELEGRAM_REGEX
@@ -35,7 +25,7 @@ class Student < StudentBase
 
   def initialize(last_name:, first_name:, id: nil, patronymic: nil, phone: nil, telegram: nil, email: nil, git: nil)
     raise ArgumentError, "Incorrect git entry" unless git.nil? or self.class.valid_git?(git)
-    super(id: id.to_i, git: git)
+    super(id: id, git: git)
     self.last_name= last_name
     self.first_name= first_name
     self.patronymic= patronymic
@@ -43,31 +33,7 @@ class Student < StudentBase
   end
 
 
-  # def self.init_with_hash(student)
-  #   raise ArgumentError, "Expected Hash, given #{student.class}" unless student.is_a?(Hash)
-  #   raise ArgumentError, "Expected at least 2 keys, given #{student.length}" unless student.length >= 2
-
-  #   normalized = Hash[student.map{ |k, v| [k.to_sym, v] }]
-
-  #   valid_keys = [:last_name, :first_name, :patronymic, :phone, :telegram, :email, :git, :id]
-  #   invalid_keys = normalized.keys - valid_keys    
-        
-  #   raise ArgumentError, "Unknown type of attribute #{invalid_keys.first}" unless invalid_keys.empty? 
-
-  #   normalized_hash = {
-  #     last_name: normalized[:last_name],
-  #     first_name: normalized[:first_name],
-  #     id: normalized[:id].to_i,
-  #     patronymic: normalized[:patronymic],
-  #     phone: normalized[:phone],
-  #     telegram: normalized[:telegram],
-  #     email: normalized[:email],
-  #     git: normalized[:git]
-  #   } 
-  #   new(**normalized_hash)
-
-  # end
-  #
+  
   def contact=(contacts)
 		raise ArgumentError, "Expected Hash, given #{contacts.class}" unless contacts.is_a?(Hash)
     raise ArgumentError, "Expected 3 contacts, given #{contacts.length}" if contacts.length != 3 
@@ -100,13 +66,7 @@ class Student < StudentBase
     return "#{@last_name} #{@first_name[0]}. #{@patronymic[0]}."
   end
 
-  # def <=>(other)
-  #   if other.nil? or other.class != Student
-  #     raise ArgumentError, "Could not compare Student with #{nil ? other.nil? : other.class}"
-  #   end
-  #   self.last_name <=> other.last_name
-  # end	
-
+  
   def to_s
     result = "#{id}" 
     result += "\nfio - #{last_name} #{first_name} #{patronymic || 'нет'} "
@@ -117,9 +77,7 @@ class Student < StudentBase
     return result
   end
 
-  # def to_h
-  #   {id: @id, last_name: last_name, first_name: first_name, patronymic: patronymic, phone: @phone, email: @email, telegram: @telegram, git: git}
-  # end
+ 
   private
   attr_validate_writer :phone, field_name: "phone", required: false, with: :valid_phone?
   attr_validate_writer :telegram, field_name: "telegram", required: false, with: :valid_telegram?
